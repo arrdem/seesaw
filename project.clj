36b2fa4809d775af07b97a40637f1d7bc78aff91
Reid 'arrdem' McKenzie <rmckenzie@twitter.com>
2016-07-30 23:38:12 -0700

    Update deps a bit

--- project.clj
+++ project.clj
@@ -3,35 +3,33 @@
 
   :url "http://seesaw-clj.org"
 
-  :mailing-list {:name "seesaw-clj"
+  :mailing-list {:name    "seesaw-clj"
                  :archive "https://groups.google.com/group/seesaw-clj"
-                 :post "seesaw-clj@googlegroups.com"}
+                 :post    "seesaw-clj@googlegroups.com"}
 
-  :license {:name "Eclipse Public License - v 1.0"
-            :url "http://www.eclipse.org/legal/epl-v10.html"
+  :license {:name         "Eclipse Public License - v 1.0"
+            :url          "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
-            :comments "same as Clojure"}
+            :comments     "same as Clojure"}
 
   :warn-on-reflection true
 
-  ; To run the examples:
-  ;
-  ;   $ lein examples
-  ;
-  :aliases { "examples" ["run" "-m" "seesaw.test.examples.launcher"] }
+  ;; To run the examples:
+  ;;
+  ;;   $ lein examples
+  ;;
+  :aliases {"examples" ["run" "-m" "seesaw.test.examples.launcher"] }
 
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [com.miglayout/miglayout "3.7.4"]
-                 [com.jgoodies/forms "1.2.1"]
-                 [org.swinglabs.swingx/swingx-core "1.6.3"]
+                 [com.jgoodies/forms "1.3.0"]
+                 [org.swinglabs.swingx/swingx-core "1.6.5-1"]
                  [j18n "1.0.2"]
-                 [com.fifesoft/rsyntaxtextarea "2.5.6"]]
-  :profiles { :dev {:dependencies [[com.stuartsierra/lazytest "1.1.2"]
+                 [com.fifesoft/rsyntaxtextarea "2.5.8"]]
+  :profiles {:dev {:dependencies [[com.stuartsierra/lazytest "1.1.2"]
                                   [lein-autodoc "0.9.0"]]}}
   :repositories [["stuartsierra-releases" "http://stuartsierra.com/maven2"]]
-  :autodoc {
-    :name "Seesaw",
-    :page-title "Seesaw API Documentation"
-    :copyright "Copyright 2012, Dave Ray" }
+  :autodoc {:name       "Seesaw",
+            :page-title "Seesaw API Documentation"
+            :copyright  "Copyright 2012, Dave Ray" }
   :java-source-paths ["jvm"])
-
